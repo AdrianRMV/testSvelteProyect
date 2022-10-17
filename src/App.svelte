acad3925c3080a031dc4b1b067460b563904a78a
Adrian <adrianthelawx@gmail.com>
2022-10-17 16:32:50 -0600

    Routes svelte

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Router, Route, Link, link } from 'svelte-routing';
+	import Login from './views/Login.svelte'
+	import Home from './views/Home.svelte'
+</script>
+
+<Router>
+	<nav>
+		<Link to='/'>Login</Link>
+		<Link to='/home'>Home</Link>
+	</nav>
+	<div>
+		<Route path="/" component={Login} />
+		<Route path="/home" component={Home} />
+	</div>
+</Router>

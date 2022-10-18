400d80af69b2f38ba8843fbe4b1f3ebd0e72f1eb
Adrian <adrianthelawx@gmail.com>
2022-10-18 11:12:35 -0600

    Estructura finished

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
-    import { Router, Route, Link, link } from 'svelte-routing';
-	import Login from './views/Login.svelte'
-	import Home from './views/Home.svelte'
+    import { Router, Route } from 'svelte-routing';
+    import Login from './views/Login.svelte';
+    import Home from './views/Home.svelte';
+    import NotFound from './views/NotFound.svelte';
 </script>
 
 <Router>
-	<div>
-		<Route path="/" component={Login} />
-		<Route path="/home" component={Home} />
-	</div>
+    <div>
+        <!-- Vistas direccionadas con componentes -->
+        <Route path="/" component={Login} />
+        <Route path="/home" component={Home} />
+        
+        <!-- 404 -->
+        <Route path="*" component={NotFound} />
+    </div>
 </Router>
--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,1 +1,2 @@
+
 <h1>Soy el home unu</h1>
